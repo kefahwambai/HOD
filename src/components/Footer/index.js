3d16aff171e3b1a2e2ff83b9ab47aa26822c89aa
KefahWambai <kefah.ngwei@student.moringaschool.com>
2024-02-24 13:10:59 +0300

    changed footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,106 +1,118 @@
 import React from 'react'
 import "./footer.css"
+import { Input, initMDB } from "mdb-ui-kit";
+
+// initMDB({ Input });
 
 export default function Footer() {
+    let date = new Date();
+    let year = date.getFullYear();
   return (
     <div>
-        <footer className="footer-section">
-        <div className="container">
-            <div className="footer-cta pt-5 pb-5">
-                <div className="row">
-                    <div className="col-xl-4 col-md-4 mb-30">
-                        <div className="single-cta">
-                            <i className="fas fa-map-marker-alt"></i>
-                            <div className="cta-text">
-                                <h4>Find us</h4>
-                                <span>1010 Avenue, sw 54321, chandigarh</span>
-                            </div>
+        <footer style={{ backgroundColor: 'grey'}} class="text-center">
+  
+            <div class="container p-4">
+        
+                <section className="mb-4 text-center">
+                    <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button">
+                        <i class="fab fa-google"></i>
+                    </a>
+
+                    <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+
+                    <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" role="button">
+                        <i class="fab fa-github"></i></a>
+                </section>
+                <section class="">
+                <form action="">
+                    <div class="row d-flex justify-content-center">           
+                    <div class="col-auto">
+                        <p class="pt-2">
+                        <strong>Sign up for our newsletter</strong>
+                        </p>
+                    </div>
+                    <div class="col-md-5 col-12">                
+                        <div data-mdb-input-init class="form-outline mb-4">
+                        <input style={{ backgroundColor: 'white'}}  type="email" id="form5Example24" class="form-control" />
+                        <label class="form-label" for="form5Example24">Email address</label>
                         </div>
                     </div>
-                    <div className="col-xl-4 col-md-4 mb-30">
-                        <div className="single-cta">
-                            <i className="fas fa-phone"></i>
-                            <div className="cta-text">
-                                <h4>Call us</h4>
-                                <span>9876543210 0</span>
-                            </div>
-                        </div>
+                    <div class="col-auto">
+                        <button data-mdb-ripple-init type="submit" class="btn btn-outline mb-4">
+                        Subscribe
+                        </button>
                     </div>
-                    <div className="col-xl-4 col-md-4 mb-30">
-                        <div className="single-cta">
-                            <i className="far fa-envelope-open"></i>
-                            <div className="cta-text">
-                                <h4>Mail us</h4>
-                                <span>mail@info.com</span>
-                            </div>
-                        </div>
                     </div>
-                </div>
-            </div>
-            <div className="footer-content pt-5 pb-5">
-                <div className="row">
-                    <div className="col-xl-4 col-lg-4 mb-50">
-                        <div className="footer-widget">
-                            <div className="footer-logo">
-                                <a href="index.html"><img src="" className="img-fluid" alt="logo"/></a>
-                            </div>
-                            <div className="footer-text">
-                                <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing
-                                elit,Lorem ipsum dolor sit amet.</p>
-                            </div>
-                            <div className="footer-social-icon">
-                                <span>Follow us</span>
-                                <a href="#"><i className="fab fa-facebook-f facebook-bg"></i></a>
-                                <a href="#"><i className="fab fa-twitter twitter-bg"></i></a>
-                                <a href="#"><i className="fab fa-google-plus-g google-bg"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
-                        <div className="footer-widget">
-                            <div className="footer-widget-heading">
-                                <h3>Useful Links</h3>
-                            </div>
-                            <ul>
-                               
-                                <li><a href="#">About us</a></li>
-                                <li><a href="#">Our Services</a></li>
-                                <li><a href="#">Contact us</a></li>
-                                <li><a href="#">FAQ</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
-                        <div className="footer-widget">
-                            <div className="footer-widget-heading">
-                                <h3>Join Our Mailing List</h3>
-                            </div>
-                            <div className="footer-text mb-25">
-                                <p>Receive invitations, updates and bonuses for purchasing.</p>
-                            </div>
-                            <div className="subscribe-form">
-                                <form action="#">
-                                    <input type="text" placeholder="Email Address"/>
-                                    <button><i className="fab fa-telegram-plane"></i></button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="copyright-area">
-            <div class="container">
+                </form>
+                </section>
+                <section class="mb-4">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+                    repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam eum
+                    harum corrupti dicta, aliquam sequi voluptate quas.
+                </p>
+                </section>
+                <section class="">
                 <div class="row">
-                    <div class="col-xl-6 col-lg-6 text-center text-lg-left">
-                        <div class="copyright-text">
-                            <p>Copyright &copy; 2024, All Right Reserved <a href="https://kefahwambai.com/">Kefah Wambai</a></p>
-                        </div>
-                    </div>                    
+                    
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                        <a class="text-body" href="#!">Link 1</a>
+                        </li>
+                        <li>
+                        <a class="text-body" href="#!">Link 2</a>
+                        </li>
+                        <li>
+                        <a class="text-body" href="#!">Link 3</a>
+                        </li>
+                        <li>
+                        <a class="text-body" href="#!">Link 4</a>
+                        </li>
+                    </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">Links</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                        <a class="text-body" href="#!">Link 1</a>
+                        </li>
+                        <li>
+                        <a class="text-body" href="#!">Link 2</a>
+                        </li>
+                        <li>
+                        <a class="text-body" href="#!">Link 3</a>
+                        </li>
+                        <li>
+                        <a class="text-body" href="#!">Link 4</a>
+                        </li>
+                    </ul>
+                    </div>
+                    
                 </div>
+                </section>
             </div>
-        </div>
-    </footer>
+            <div class="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.05)"}}>
+                 © {year} Copyright:{' '}
+                <a class="text-reset fw-bold" href="https://kefahwambai.com/" target="_blank">
+                  kefahwambai.com
+                </a>
+            </div>
+        </footer>
     </div>
   )
 }
